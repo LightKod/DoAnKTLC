4ccf317ebb608ecd0f1e1112c94fdbb7780681d9
lucky634634 <khoa21102003@gmail.com>
2022-02-04 20:28:22 +0700

    Revert "Add move+Thread"
    This reverts commit 43371c9e2393cb153600511d8e8e03f8ef740bc3.

--- Snake_Nhom14/Snake_Nhom14/Gameplay.h
+++ Snake_Nhom14/Snake_Nhom14/Gameplay.h
@@ -1,28 +1,11 @@
 #pragma once
 #include <Windows.h>
-#include "Graphic.h"
 
-static struct POINT
-{
-	int x;
-	int y;
-};
-POINT snake[100], food[4];
-static int SIZE_SNAKE = 0; 
-int CHAR_LOCK; //used to determine the direction my snake cannot move
-int MOVING; //used to determine the direction my snake moves
-static int SPEED; //snake's speed
-int HEIGH_CONSOLE, WIDTH_CONSOLE; //height and width of console-screen
-int FOOD_INDEX; //current food-index
-int STATE; //State of snake: dead or alive
+static POINT snake[100];
+static int snakeSize = 0;
+static POINT food;
 
 bool IsValid(int x, int y);
 void GenerateFood();
 void TestFoodSpawn();
-void MoveRight();
-void MoveLeft();
-void MoveUp();
-void MoveDown();
-void ThreadFunc();
-void ProcessDead();
-void Eat();
+
