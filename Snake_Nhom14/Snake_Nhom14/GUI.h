791fd9f09dcbc25ed4ffcd004f6e3ff7f324e647
ngocvu9412 <98596186+ngocvu9412@users.noreply.github.com>
2022-02-12 12:10:35 +0700

    Update GUI.h

--- Snake_Nhom14/Snake_Nhom14/GUI.h
+++ Snake_Nhom14/Snake_Nhom14/GUI.h
@@ -1,12 +1,4 @@
 #include <Windows.h>
-
-static bool playingGame = true;
-static bool howtoplay = false;
-static bool highscore = false;
-static bool exitGame = false;
-static bool exitStartMenu = false;
-static POINT cursor = { 59, 19 };
-
 void DisplaySNAKE();
 void DisplayGAME();
 void DisplaySNAKEIMG();
