4564476e7708e0e41793bd8124839b08ae2f5275
ngocvu9412 <98596186+ngocvu9412@users.noreply.github.com>
2022-02-12 12:05:43 +0700

    Update GUI.h

--- Snake_Nhom14/Snake_Nhom14/GUI.h
+++ Snake_Nhom14/Snake_Nhom14/GUI.h
@@ -6,9 +6,12 @@ static bool highscore = true;
 static bool exitGame = false;
 static bool optionMenu = false;
 static bool exitStartMenu = false;
-static POINT cursor = { 0, 0 };
+static POINT cursor = { 59, 19 };
 
-void OptionMenu();
+void DisplaySNAKE();
+void DisplayGAME();
+void DisplaySNAKEIMG();
+void MenuBG();
 void GUI();
 void GameplayUI();
 void ClearScreen();
