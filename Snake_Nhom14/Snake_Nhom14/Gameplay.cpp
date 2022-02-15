82ddd87822c3c3d0c3ab10b3664aa02de828bca5
Huy203 <95161186+Huy203@users.noreply.github.com>
2022-02-16 00:20:55 +0700

    Upgrade level

--- Snake_Nhom14/Snake_Nhom14/Gameplay.cpp
+++ Snake_Nhom14/Snake_Nhom14/Gameplay.cpp
@@ -475,16 +475,16 @@ void GenerateWall()
 	int x, y;
 	do
 	{
-		x = rand() % (game_field_width - 2) + game_field_pos.x + 1;
-		y = rand() % (game_field_height - 2) + game_field_pos.y + 1;
+	x = rand() % (game_field_width - 2) + game_field_pos.x + 1;
+	y = rand() % (game_field_height - 2) + game_field_pos.y + 1;
 	} while (!IsValid(x, y) || IsAlreadyHad(x, y));
 	wall_size++;
-	wall_pos[wall_size - 1] = {x, y};
+	wall_pos[wall_size - 1] = { x, y };
 }
 
 void GenerateWallNew()
 {
-	switch ((level - 1) % 3 + 1)
+	switch ((level))
 	{
 	case 1: // Level 1: 0 wall
 		wall_size = 0;
@@ -492,69 +492,104 @@ void GenerateWallNew()
 	case 2: // Level 2: 4 cuc vuong o 4 goc
 		wall_size = 16;
 		// Top Right
-		wall_pos[0] = {5, 5};
-		wall_pos[1] = {6, 5};
-		wall_pos[2] = {6, 6};
-		wall_pos[3] = {5, 6};
+		wall_pos[0] = { 5, 5 };
+		wall_pos[1] = { 6, 5 };
+		wall_pos[2] = { 6, 6 };
+		wall_pos[3] = { 5, 6 };
 
 		// Top Left
-		wall_pos[4] = {42 - 5, 5};
-		wall_pos[5] = {42 - 6, 5};
-		wall_pos[6] = {42 - 6, 6};
-		wall_pos[7] = {42 - 5, 6};
+		wall_pos[4] = { 42 - 5, 5 };
+		wall_pos[5] = { 42 - 6, 5 };
+		wall_pos[6] = { 42 - 6, 6 };
+		wall_pos[7] = { 42 - 5, 6 };
 
 		// Bottom Left
-		wall_pos[8] = {42 - 5, 42 - 5};
-		wall_pos[9] = {42 - 6, 42 - 5};
-		wall_pos[10] = {42 - 6, 42 - 6};
-		wall_pos[11] = {42 - 5, 42 - 6};
+		wall_pos[8] = { 42 - 5, 42 - 5 };
+		wall_pos[9] = { 42 - 6, 42 - 5 };
+		wall_pos[10] = { 42 - 6, 42 - 6 };
+		wall_pos[11] = { 42 - 5, 42 - 6 };
 
 		// Bottom Right
-		wall_pos[12] = {5, 42 - 5};
-		wall_pos[13] = {6, 42 - 5};
-		wall_pos[14] = {6, 42 - 6};
-		wall_pos[15] = {5, 42 - 6};
+		wall_pos[12] = { 5, 42 - 5 };
+		wall_pos[13] = { 6, 42 - 5 };
+		wall_pos[14] = { 6, 42 - 6 };
+		wall_pos[15] = { 5, 42 - 6 };
 		break;
 
 	case 3:
 		wall_size = 28;
 
 		// Top left
-		wall_pos[0] = {6, 3};
-		wall_pos[1] = {6, 4};
-		wall_pos[2] = {6, 5};
-		wall_pos[3] = {6, 6};
-		wall_pos[4] = {5, 6};
-		wall_pos[5] = {4, 6};
-		wall_pos[6] = {3, 6};
+		wall_pos[0] = { 6, 3 };
+		wall_pos[1] = { 6, 4 };
+		wall_pos[2] = { 6, 5 };
+		wall_pos[3] = { 6, 6 };
+		wall_pos[4] = { 5, 6 };
+		wall_pos[5] = { 4, 6 };
+		wall_pos[6] = { 3, 6 };
 
 		// Top right
-		wall_pos[7] = {38, 3};
-		wall_pos[8] = {38, 4};
-		wall_pos[9] = {38, 5};
-		wall_pos[10] = {38, 6};
-		wall_pos[11] = {39, 6};
-		wall_pos[12] = {40, 6};
-		wall_pos[13] = {41, 6};
+		wall_pos[7] = { 38, 3 };
+		wall_pos[8] = { 38, 4 };
+		wall_pos[9] = { 38, 5 };
+		wall_pos[10] = { 38, 6 };
+		wall_pos[11] = { 39, 6 };
+		wall_pos[12] = { 40, 6 };
+		wall_pos[13] = { 41, 6 };
 
 		// Bottom right
-		wall_pos[14] = {38, 41};
-		wall_pos[15] = {38, 40};
-		wall_pos[16] = {38, 39};
-		wall_pos[17] = {38, 38};
-		wall_pos[18] = {39, 38};
-		wall_pos[19] = {40, 38};
-		wall_pos[20] = {41, 38};
+		wall_pos[14] = { 38, 41 };
+		wall_pos[15] = { 38, 40 };
+		wall_pos[16] = { 38, 39 };
+		wall_pos[17] = { 38, 38 };
+		wall_pos[18] = { 39, 38 };
+		wall_pos[19] = { 40, 38 };
+		wall_pos[20] = { 41, 38 };
 
 		// Bottom left
-		wall_pos[21] = {6, 41};
-		wall_pos[22] = {6, 40};
-		wall_pos[23] = {6, 39};
-		wall_pos[24] = {6, 38};
-		wall_pos[25] = {5, 38};
-		wall_pos[26] = {4, 38};
-		wall_pos[27] = {3, 38};
+		wall_pos[21] = { 6, 41 };
+		wall_pos[22] = { 6, 40 };
+		wall_pos[23] = { 6, 39 };
+		wall_pos[24] = { 6, 38 };
+		wall_pos[25] = { 5, 38 };
+		wall_pos[26] = { 4, 38 };
+		wall_pos[27] = { 3, 38 };
 		break;
+
+	case 4: {
+		wall_size = 90;
+
+		// Outside
+		int i = 0;
+		for (int temp1 = 8, temp2 = 8; i < (wall_size - 30) / 4; i++)
+		{
+			wall_pos[i] = { temp1, temp2 };
+			wall_pos[90 - i - 1] = { 44 - temp1,44 - temp2 };
+			i++;
+			wall_pos[i] = { 44 - temp1, temp2 };
+			wall_pos[wall_size - i - 1] = { temp1, 44 - temp2 };
+			if (i < 7) temp1++;
+			else {
+				temp1 = 8; temp2++;
+			}
+		}
+
+		//Inside
+		for (int temp1 = 13, temp2 = 16; i < (wall_size / 2); i++)
+		{
+			wall_pos[i] = { temp1, temp2 };
+			wall_pos[90 - i - 1] = { 44 - temp1,44 - temp2 };
+			i++;
+			wall_pos[i] = { 44 - temp1, temp2 };
+			wall_pos[wall_size - i - 1] = { temp1, 44 - temp2 };
+			if (i < (wall_size / 2) - 5) temp1++;
+			else {
+				temp1 = 13; temp2++;
+			}
+		}
+
+		break;
+	}
 	}
 }
 
@@ -592,6 +627,14 @@ void SetGateCollider()
 		gate_colliders[2] = {gate_pos.x - 1, gate_pos.y};
 		gate_colliders[3] = {gate_pos.x - 1, gate_pos.y + 1};
 		gate_colliders[4] = {gate_pos.x, gate_pos.y + 1};
+		break;
+
+	case 4:
+		gate_colliders[0] = { gate_pos.x - 1, gate_pos.y - 1 };
+		gate_colliders[1] = { gate_pos.x - 1, gate_pos.y - 1 };
+		gate_colliders[2] = { gate_pos.x - 1, gate_pos.y };
+		gate_colliders[3] = { gate_pos.x - 1, gate_pos.y + 1 };
+		gate_colliders[4] = { gate_pos.x, gate_pos.y + 1 };
 		break;
 	}
 }
